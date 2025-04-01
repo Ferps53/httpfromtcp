08053021345e89e0e3254dbf44f6551026f21033
Ferps53 <felipebrostolinribeiro@gmail.com>
2025-03-31 22:18:13 -0300

    Refactored code to use channels

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
 
 func main() {
-
 	file, err := os.Open("./messages.txt")
 
 	if err != nil {
@@ -15,14 +15,33 @@ func main() {
 		return
 	}
 
+	channel := getLinesChannel(file)
+
+	defer file.Close()
+
+	for line := range channel {
+		fmt.Printf("read: %s\n", line)
+	}
+}
+
+func getLinesChannel(file io.ReadCloser) <-chan string {
+
+	channel := make(chan string)
+	go readLines(file, channel)
+
+	return channel
+}
+
+func readLines(file io.ReadCloser, channel chan string) {
+
+	defer close(channel)
 	var line string
 	for {
 
 		data := make([]byte, 8)
 		count, err := file.Read(data)
-
 		if err != nil {
-			os.Exit(0)
+			return
 		}
 
 		parts := strings.Split(string(data[:count]), "\n")
@@ -30,7 +49,7 @@ func main() {
 		line += parts[0]
 
 		if len(parts) == 2 {
-			fmt.Printf("read: %s\n", line)
+			channel <- line
 			line = parts[1]
 		}
 	}
